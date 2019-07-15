c09b167e315790cf51039be21b7cdb8ad47b6776
Desmond <desmond@desmondchin.xyz>
2019-07-15 13:44:32 +0800

    initial

---
+++ food.js
@@ -0,0 +1,76 @@
+var brunch = [
+    "Shin Lian Sin (Kolo Mee)",
+    "Fu Yu (Laksa)",
+    "Chong Choon (Laksa)",
+    "Thompson Corner Tabuan Jaya (Laksa)",
+    "Lau Ya Keng (Tang Hoon Fishball)",
+    "Kim Joo",
+    "Joo Seng (Bread + Teh Tarik Peng)",
+    "Best Corner 101 (Sabah Pork Noodle)",
+    "Wan Xiang (Sabah Pork Noodle)",
+    "Kwong Hup (Penang Fried Kueh Tiao/Porridge)",
+    "Ming Hong Kee (Porridge)",
+    "No.6 (Lamb Soup + Rice)",
+    "Yum Yum (Chicken Rice)",
+    "I.P.H Roasted",
+    "Curry House",
+    "King’s Curry",
+    "Morgan’s Kueh Tiao Pork Soup",
+    "Song Corner (American Breakfast)",
+    "Meng Kui (Heng Hua Noodle)",
+    "Xiang Man Low (Bak Kut Teh)",
+    "Goh Fun Kee (Bak Kut Teh)",
+    "Muara Tebas (right corner shop)",
+    "Bako Seafood",
+    "Boulevard Restaurant"
+]
+
+var western = [
+    "Ken’s Kitchen",
+    "Hilltop House",
+    "Manna Western Food",
+    "Mike Smokehouse (Bacon Aglio Olio)",
+    "Junk",
+    "Indoshiok",
+    "Cafe Cafe (Fusion)",
+    "Razzmatazz",
+    "Zinc",
+    "Alfresco",
+    "Nandos",
+    "Meat Up",
+    "Farm’s Bistro",
+    "Howdy’s (Pork Ribs)",
+    "Petanak Market"
+]
+
+var fastfood = [
+    "KFC",
+    "McD",
+    "Hartz",
+    "Sugarbun",
+    "Chicago.7"
+]
+
+var japanese = [
+    "Sando Ramen",
+    "Rising Ramen",
+    "Sushi Zanmai",
+    "Sushi King (Gyudon)",
+    "Minoru",
+    "Oishi",
+]
+
+var korean = [
+    "Daol Korean Brunch Cafe & Bar",
+    "Pelicana"
+]
+
+var chinese = [
+    "Jong’s Seafood",
+    "Lintang Park",
+    "Stapok Seafood",
+    "Bukit Mata",
+    "Boulevard Restaurant",
+    "Heritage",
+    "Country Kitchen"
+]
